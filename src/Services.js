35d93f8e989e12b73ae23bb1e121fcc6d26f283e
Arian Pourhossein <Cod16duty@gmail.com>
2024-04-28 21:24:47 +0330

    Clear Coding

---
+++ src/Services.js
@@ -0,0 +1,27 @@
+const services = [
+  {
+    title: "Read Free Library Books Online",
+    description:
+      "Millions of books available through Controlled Digital Lending",
+    img: "../../public/Pics/Service1.jpg",
+    leftWork: "More Information",
+    rightWork: "View The Books",
+  },
+  {
+    title: "Set a Yearly Reading Goal",
+    description:
+      "Learn how to set a yearly reading goal and track what you read",
+    img: "../../public/Pics/Service2.jpg",
+    leftWork: "More Information",
+    rightWork: "Set The Goal",
+  },
+  {
+    title: "Keep Track of your Favorite Books",
+    description: " Organize your Books using Lists & the Reading Log",
+    img: "../../public/Pics/Service3.jpg",
+    leftWork: "More Information",
+    rightWork: "Favorite List",
+  },
+
+];
+export default services;
