a466f5b35a9ce15854044b042d81e76fc514da23
Arian Pourhossein <Cod16duty@gmail.com>
2024-05-07 19:10:31 +0330

    Start Creating Calendar

--- src/Components/Calender.jsx
+++ src/Components/Calender.jsx
@@ -1,39 +1,115 @@
 import { useState } from "react";
+import Box from "@mui/material/Box";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+import {
+  usePickerLayout,
+  pickersLayoutClasses,
+  PickersLayoutRoot,
+  PickersLayoutContentWrapper,
+} from "@mui/x-date-pickers/PickersLayout";
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
+import { useMediaQuery } from "react-responsive";
 import "../Styles/Calendar.css";
-function Calender() {
-  const [isshow, setIsShow] = useState(true);
-  const Toggle = () => {
-    setIsShow(!isshow);
-  };
+function ActionList(props) {
+  // eslint-disable-next-line react/prop-types
+  const { onAccept, onClear, onCancel, onSetToday } = props;
+  const actions = [
+    { text: "Accept", method: onAccept },
+    { text: "Clear", method: onClear },
+    { text: "Cancel", method: onCancel },
+    { text: "Today", method: onSetToday },
+  ];
+
   return (
-    <section id="Calender">
-      <hr />
-      <div
-        className={`${
-          isshow
-            ? " animate__animated animate__rollIn"
-            : " animate__animated animate__rollOut"
-        }`}
+    <List>
+      {actions.map(({ text, method }) => (
+        <ListItem key={text} disablePadding>
+          <ListItemButton onClick={method}>
+            <ListItemText primary={text} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  );
+}
+function BookHeader() {
+  return (
+    <Box
+      sx={{
+        // Place the element in the grid layout
+        gridColumn: 1,
+        gridRow: 1,
+        // Center the icon
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <FontAwesomeIcon icon={faBook} />
+      <h3>Pick Your DATE !</h3>
+    </Box>
+  );
+}
+function CustomLayout(props) {
+  const { toolbar, tabs, content, actionBar } = usePickerLayout(props);
+
+  return (
+    <PickersLayoutRoot
+      ownerState={props}
+      sx={{
+        [`.${pickersLayoutClasses.actionBar}`]: {
+          gridColumn: 1,
+          gridRow: 2,
+        },
+        [`.${pickersLayoutClasses.toolbar}`]: {
+          gridColumn: 2,
+          gridRow: 1,
+        },
+      }}
+    >
+      <BookHeader />
+      {toolbar}
+      {actionBar}
+      <PickersLayoutContentWrapper
+        className={pickersLayoutClasses.contentWrapper}
       >
-        <h3>This Feauture Is Not Available Right now ! 😕</h3>
-        <span>-</span>
-        <p>
-          📑The function that will be placed here will be Like This :<br />{" "}
-          After you choose the Date in this calendar, the books whose
-          information is taken from the API, their publication time will be
-          matched with the time you chose and then it will be shown to you.{" "}
-          <br />
-          -UseEffect | Needed !
-          <br />
-          <span>
-            Therefore, until learning the desired topic, this feature will be
-            inactive.{" "}
-          </span>
-        </p>
-      </div>
-      <button onClick={Toggle}>{isshow ? "Hide" : "Show"}</button>
-      <hr />
-    </section>
+        {tabs}
+        {content}
+      </PickersLayoutContentWrapper>
+    </PickersLayoutRoot>
+  );
+}
+function Calender() {
+  const MobileQu = useMediaQuery({
+    query: "(max-width: 500px)",
+  });
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <section id="Calender">
+        <hr />
+
+        {MobileQu ? (
+          <MobileDatePicker />
+        ) : (
+          <StaticDatePicker
+            slots={{
+              layout: CustomLayout,
+              actionBar: ActionList,
+            }}
+          />
+        )}
+
+        <hr />
+      </section>
+    </LocalizationProvider>
   );
 }
 
